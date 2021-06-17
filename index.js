5b6f962bcd11ef976e30f8b274de1c4073f1282b
dross3121 <dross3121@gmail.com>
2021-06-17 14:10:02 -0400

    working on put route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const methodOverride = require('method-override')
 const app = express();
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
 const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
@@ -9,14 +9,14 @@ const Tickets = require('./models/tickets-model')
 require('dotenv/config');
 
 
-// settign render templating as ejs
+// setting render templating as ejs
 app.set('view engine', 'ejs');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
 
 
 
@@ -27,6 +27,7 @@ app.use(methodOverride('_method'))
 app.get('/tickets/', (req, res) => {
     Tickets.find({})
     .then((tickets) =>{
+        console.log(tickets)
         res.render('index', {tickets : tickets})
     })
     .catch(console.error);
@@ -43,7 +44,10 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now())
     }
 });
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    } });
 
 // add /new route for this form to create new tickets 
 app.get('/tickets/new/', (req,res) =>{
@@ -63,7 +67,8 @@ app.post('/tickets/', upload.single('image'), (req, res, next) => {
     .then(ticket => {
         console.log(ticket)
         res.redirect('/tickets')
-    });
+    })
+    .catch(console.error);
 });
 
 // get one ticket by id route
@@ -73,14 +78,45 @@ app.get('/tickets/:id', (req,res) =>{
     Tickets.findById(id)
     .then((ticket) =>{
        res.render('show', {ticket : ticket})
-       return ticket
     })
-
+    .catch(console.error);
 })
-// []"PUT" update based from id
-// app.put('')
 
+// [x] grab a ticket by id tand render to the PUT to update
+app.get('/tickets/:id/edit', (req,res) =>{
+    let id = req.params.id
+    console.log(id)
+    Tickets.findById(id)
+    .then((ticket) =>{
+       res.render('edit', {ticket : ticket})
+    })
+})
 
+// []  route
+app.put('/tickets/:id', (req,res) =>{
+    let id = req.params.id
+    let filename = req.body.img
+    console.log(id)
+    Tickets.findOneAndUpdate(
+        {_id : id},
+        {
+            title: req.body.title,
+            desc: req.body.desc,
+            offer: req.body.offer,
+            // [] need to figure why it sayig path name undefined
+            // return to show route
+            img: {
+                data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+                contentType: 'image/png'
+                }, 
+        },
+        {new : true}
+        )
+        .then((ticket) =>{
+       res.render('show', {ticket : ticket})
+        })
+    .catch(console.error)
+})
 
 
 
@@ -93,3 +129,13 @@ app.listen(app.get("port"), () => {
     console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
 
+// TODOs
+// [] finsh put and delete route
+// [] flash message for when picture is to large
+// [] connect user and tickets in database
+// [] style.css or bootstrap
+// [] fix heroku 
+// [] re-factor routes
+// [] user login/ authentication
+// [] rendering all of a users tickets based on id
+// [] 
